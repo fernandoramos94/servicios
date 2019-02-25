faa9497f730eca164a8796ad52effae07a1b330a
fernandoramos94 <ferrati94100112668@gmail.com>
2019-02-24 19:45:39 -0500

    service for by client

--- application/app/Http/Controllers/Api/ServicesController.php
+++ application/app/Http/Controllers/Api/ServicesController.php
@@ -35,14 +35,46 @@ class ServicesController extends Controller
         ->join('shop_img', 'shop.id', '=', "shop_img.id_shop")
         ->join('shop_categorie', 'shop.id', '=', "shop_categorie.id_shop")
         ->join('categorie', 'shop_categorie.id_categorie', '=', "categorie.id")
-        // ->select()
+        ->select(
+            'shop.descripcion', 
+            'shop.id as id', 
+            'categorie.categoria as categoria', 
+            'shop.empresa as empresa',
+            'shop.nit as nit',
+            'shop.telefono as telefono',
+            'shop.direccion as direccion',
+            'shop.nombre as nombre',
+            'shop.celular as celular',
+            'shop.correo as correo',
+            'shop.redes_sociales as redes',
+            'shop_img.url_imagen as url_imagen',
+            'shop_img.url_logo as url_logo'
+        )
         ->get();
         return response()->json($barrios);
     }
     public function getByClient($id)
     {
-        $byCliente = DB::table("shop")
-        ->where("id", "=", $id)
+        $byCliente =DB::table('shop')
+        ->join('shop_img', 'shop.id', '=', "shop_img.id_shop")
+        ->join('shop_categorie', 'shop.id', '=', "shop_categorie.id_shop")
+        ->join('categorie', 'shop_categorie.id_categorie', '=', "categorie.id")
+        ->select(
+            'shop.descripcion', 
+            'shop.id as id', 
+            'categorie.categoria as categoria', 
+            'shop.empresa as empresa',
+            'shop.nit as nit',
+            'shop.telefono as telefono',
+            'shop.direccion as direccion',
+            'shop.nombre as nombre',
+            'shop.celular as celular',
+            'shop.correo as correo',
+            'shop.redes_sociales as redes',
+            'shop_img.url_imagen as url_imagen',
+            'shop_img.url_logo as url_logo'
+        )
+        ->where('shop.id', "=" $id)
         ->first();
 
         return response()->json($byCliente);
